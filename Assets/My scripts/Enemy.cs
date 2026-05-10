7dc14c9abd07e56075e6b2f3c193da248051e667
Abodi37 <abdullahaljamaan37@gmail.com>
2026-05-10 20:35:23 +0300

    I added some new stuff in the Enemy script
    I added that the enemy looks at the player all the time with an optional script if there's a problem in the one that I'm using

--- Assets/My scripts/Enemy.cs
+++ Assets/My scripts/Enemy.cs
@@ -7,10 +7,32 @@ public class Enemy : MonoBehaviour
     public float maxHealth = 100f;
 
     private EnemyManager manager;
+    public Transform playerTransform;
 
     void Start()
     {
         manager = FindFirstObjectByType<EnemyManager>();
+        if (playerTransform == null)
+        {
+            playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
+        }
+    }
+    void Update()
+    {
+        if (playerTransform != null)
+        {
+        // 1. Make the enemy look exactly at the player
+        transform.LookAt(playerTransform);
+
+        // 2. OPTIONAL: If the enemy tilts weirdly (like leaning forward), 
+        // use this instead to only rotate on the Y axis (staying upright):
+        /*
+        Vector3 targetPostition = new Vector3(playerTransform.position.x, 
+                                            this.transform.position.y, 
+                                            playerTransform.position.z);
+        this.transform.LookAt(targetPostition);
+        */
+        }
     }
 
     public void TakeDamage(float amount)
